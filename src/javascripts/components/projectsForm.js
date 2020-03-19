826feaf843ef68d4e8e0f3d2d679894442c41464
Todd Spainhour <toddspainhour@gmail.com>
2020-03-18 23:44:59 -0500

    project cards printing to page functional

--- src/javascripts/components/projectsForm.js
+++ src/javascripts/components/projectsForm.js
@@ -5,23 +5,20 @@ import projectInfo from '../helpers/data/projectData';
 const buildProjectCards = () => {
   let domString = '';
   const projects = projectInfo.getProjects();
-  // projects.getProjects().forEach((project) => {
   projects.forEach((project) => {
     if (project.available === true) {
       domString += '<div class="card-body col-lg-3 bg-white" >';
-      domString += `<h2 class="text-center">${projectInfo.getProjects.title}</h2>`;
-      domString += `<img src="${projectInfo.getProjects.screenshot}">`;
-      domString += `<p class="text-center">${projectInfo.getProjects.description}</p>`;
-      domString += `<p class="text-center">${projectInfo.getProjects.technologiesUsed}</p>`;
-      domString += `<p class="text-center">${'URL: '}${projectInfo.getProjects.url}</p>`;
-      domString += `<p class="text-center">${'Github: '}${projectInfo.getProjects.githubUrl}</p>`;
+      domString += `<h2 class="text-center">${project.title}</h2>`;
+      domString += `<img src="${project.screenshot}">`;
+      domString += `<p class="text-center">${project.description}</p>`;
+      domString += `<p class="text-center">${project.technologiesUsed}</p>`;
+      domString += `<p class="text-center">${'URL: '}${project.url}</p>`;
+      domString += `<p class="text-center">${'Github: '}${project.githubUrl}</p>`;
       domString += '</div>';
     }
     util.printToDom('printProjectsHere', domString);
   });
 };
-
-// buildProjectCards();
 
 
 export default { buildProjectCards };
