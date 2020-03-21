2432db5e2b4d656738638bffd2f8400dba6a05ee
Todd Spainhour <toddspainhour@gmail.com>
2020-03-20 21:06:45 -0500

    three real projects added to project cards

--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -1,18 +1,38 @@
 const projects = [
   {
-    title: 'Project 1',
-    screenshot: 'https://thumbs.dreamstime.com/z/concept-design-training-construction-project-web-color-line-icons-collection-77263563.jpg',
-    description: 'This will be project 1',
+    title: 'Pet Adoption',
+    screenshot: 'https://raw.githubusercontent.com/ToddSpainhour/pet-adoption/master/screenshots/pet-adoption-screenshot.png',
+    description: 'In this project, I created a mock pet adoption site where the user can narrow down what type of pet they are looking for.',
     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
-    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    url: '<a href="https://github.com/ToddSpainhour/pet-adoption">Live Site</a>',
+    githubUrl: '<a href="https://github.com/ToddSpainhour/pet-adoption">See the Code</a>',
   },
 
   {
     title: 'Project 2',
-    screenshot: 'https://miro.medium.com/max/1000/1*eP_YjUdAauMdRSwWzlOl3A.png',
-    description: 'This will be project 2',
+    screenshot: 'https://raw.githubusercontent.com/ToddSpainhour/bear-watcher/master/screenshots/bear-watcher-screenshot-v1.PNG',
+    description: 'Bear Watcher',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Webpack, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    githubUrl: '<a href="https://github.com/ToddSpainhour/bear-watcher">See the Code</a>',
+  },
+
+  {
+    title: 'Project 3',
+    screenshot: 'https://raw.githubusercontent.com/ToddSpainhour/tamagotchi/master/src/screenshots/tamagotchi_v1.jpg',
+    description: 'Tamagotchi',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Webpack, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    githubUrl: '<a href="https://github.com/ToddSpainhour/tamagotchi">See the Code</a>',
+  },
+
+  {
+    title: 'Project 4',
+    screenshot: 'https://cdn.nopio.com/wp-content/uploads/2016/05/28094946/web-development.jpg',
+    description: 'This will be project 4',
     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
     url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
@@ -20,9 +40,9 @@ const projects = [
   },
 
   {
-    title: 'Project 3',
+    title: 'Project 5',
     screenshot: 'https://cdn.nopio.com/wp-content/uploads/2016/05/28094946/web-development.jpg',
-    description: 'This will be project 3',
+    description: 'This will be project 5',
     technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
     url: 'https://github.com/nss-evening-cohort-8/js-part-deux',
