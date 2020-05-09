25c12a33df78e02dcd72daba2f2c1bc884ef1e33
Todd Spainhour <toddspainhour@gmail.com>
2020-05-09 08:36:13 -0500

    links on project cards functioning properly

--- src/javascripts/components/projectsForm.js
+++ src/javascripts/components/projectsForm.js
@@ -2,42 +2,21 @@ import util from '../helpers/util';
 import projectData from '../helpers/data/projectData';
 
 
-// const buildProjectCards = () => {
-//   let domString = '';
-//   const projects = projectData.getProjects();
-//   console.error('projects', projects);
-//   projects.forEach((project) => {
-//     if (project.available === true) {
-//       domString += '<div class="card-body col-lg-3 m-1 mb-5" >';
-//       domString += `<h2 class="text-center">${project.title}</h2>`;
-//       domString += `<img src="${project.screenshot}" class="card-img">`;
-//       domString += `<p class="text-center">${project.description}</p>`;
-//       domString += `<p class="text-center">${project.technologiesUsed}</p>`;
-//       domString += `<p class="text-center">${'URL: '}${project.url}</p>`;
-//       domString += `<p class="text-center">${'Github: '}${project.githubUrl}</p>`;
-//       domString += '</div>';
-//     }
-//     util.printToDom('printProjectsHere', domString);
-//   });
-// };
-
-
 const buildProjectCards = () => {
-  // const projects = projectData.getProjects();
-  // console.error('projects', projects);
   projectData.getProjects()
     .then((projects) => {
       let domString = '';
       projects.forEach((project) => {
-      // if (project.available === true) {
-        domString += '<div class="card-body col-lg-3 m-1 mb-5" >';
-        domString += `<h2 class="text-center">${project.title}</h2>`;
-        domString += `<img src="${project.screenshot}" class="card-img">`;
-        domString += `<p class="text-center">${project.description}</p>`;
-        domString += `<p class="text-center">${project.technologiesUsed}</p>`;
-        domString += `<p class="text-center">${'URL: '}${project.url}</p>`;
-        domString += `<p class="text-center">${'Github: '}${project.githubUrl}</p>`;
-        domString += '</div>';
+        if (project.available === true) {
+          domString += '<div class="card-body col-lg-3 m-1 mb-5" >';
+          domString += `<h2 class="text-center">${project.title}</h2>`;
+          domString += `<img src="${project.screenshot}" class="card-img">`;
+          domString += `<p class="text-center">${project.description}</p>`;
+          domString += `<p class="text-center">${project.technologiesUsed}</p>`;
+          domString += `<p class="text-center"><a href="${project.url}">See the Site</a></p>`;
+          domString += `<p class="text-center"><a href="${project.githubUrl}">See the Code</a></p>`;
+          domString += '</div>';
+        }
         util.printToDom('printProjectsHere', domString);
       });
     });
