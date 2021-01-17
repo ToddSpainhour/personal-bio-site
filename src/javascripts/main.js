927f2fd615c0707b02efa433eb1c973c763eb488
Todd Spainhour <toddspainhour@gmail.com>
2021-01-16 20:48:27 -0600

    added dynamic button to show or hide resume div

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,18 @@
 import '../styles/main.scss';
 import buildProjectCards from './components/projectsForm';
 
-// const toggleResumeVisibility = () => {
-//   document.getElementById('resume-veiwer-conatainer').style.display = 'flex';
-// };
-
 const toggleResumeVisibility = () => {
   const resumeDiv = document.getElementById('resume-veiwer-conatainer');
+  const viewResumeBtn = document.getElementById('view-resume-button');
   if (resumeDiv.style.display === 'none') {
+    viewResumeBtn.innerHTML = 'Hide Resume';
+    viewResumeBtn.style.backgroundColor = '#91333C';
+    viewResumeBtn.style.color = '#C3C5cc';
     resumeDiv.style.display = 'flex';
   } else {
+    viewResumeBtn.innerHTML = 'View Resume';
+    viewResumeBtn.style.backgroundColor = '#34718C';
+    viewResumeBtn.style.color = '#26282B';
     resumeDiv.style.display = 'none';
   }
 };
