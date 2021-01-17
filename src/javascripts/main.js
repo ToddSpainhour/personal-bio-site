7c0258ca552ca7bc1326e7ddd912166984e2e74e
Todd Spainhour <toddspainhour@gmail.com>
2021-01-16 18:19:34 -0600

    resume hidden until button click

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,16 @@
 import '../styles/main.scss';
 import buildProjectCards from './components/projectsForm';
 
+const toggleResumeVisibility = () => {
+  document.getElementById('resume-veiwer-conatainer').style.display = 'flex';
+};
+
+const events = () => {
+  document.getElementById('view-resume-button').addEventListener('click', toggleResumeVisibility);
+};
+
 const init = () => {
+  events();
   buildProjectCards.buildProjectCards();
 };
 
